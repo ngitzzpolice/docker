c8761626b8615599ad77a27aca81a11f1b8e641f
Darren Stahl <darst@microsoft.com>
2016-03-07 18:40:37 -0800

    Updated Windows OCI Spec for containerd integration
    Signed-off-by: Darren Stahl <darst@microsoft.com>

--- libcontainerd/windowsoci/oci_windows.go
+++ libcontainerd/windowsoci/oci_windows.go
@@ -2,8 +2,13 @@ package windowsoci
 
 // This file is a hack - essentially a mirror of OCI spec for Windows.
 
-import "fmt"
+import (
+	"fmt"
 
+	"github.com/docker/go-connections/nat"
+)
+
+// WindowsSpec is the full specification for Windows containers.
 type WindowsSpec struct {
 	Spec
 
@@ -30,28 +35,47 @@ type Spec struct {
 	Mounts []Mount `json:"mounts"`
 	//	// Hooks are the commands run at various lifecycle events of the container.
 	//	Hooks Hooks `json:"hooks"`
-
 }
 
 // Windows contains platform specific configuration for Windows based containers.
 type Windows struct {
 	// Resources contain information for handling resource constraints for the container
 	Resources *Resources `json:"resources,omitempty"`
+	// Networking contains the platform specific network settings for the container.
+	Networking *Networking `json:"networking,omitempty"`
+	// FirstStart is used for an optimization on first boot of Windows
+	FirstStart bool `json:"first_start,omitempty"`
+	// LayerFolder is the path to the current layer folder
+	LayerFolder string `json:"layer_folder,omitempty"`
+	// Layer paths of the parent layers
+	LayerPaths []string `json:"layer_paths,omitempty"`
+	// HvRuntime contains settings specific to Hyper-V containers, omitted if not using Hyper-V isolation
+	HvRuntime *HvRuntime `json:"hv_runtime,omitempty"`
 }
 
 // Process contains information to start a specific application inside the container.
 type Process struct {
 	// Terminal creates an interactive terminal for the container.
 	Terminal bool `json:"terminal"`
-	//	// User specifies user information for the process.
-	//	User User `json:"user"`
+	// ConsoleSize contains the initial h,w of the console size
+	InitialConsoleSize [2]int `json:"-"`
+	// User specifies user information for the process.
+	User User `json:"user"`
 	// Args specifies the binary and arguments for the application to execute.
 	Args []string `json:"args"`
+	// ArgsEscaped specifies if the arguments are already escaped or still need to be escaped
+	ArgsEscaped bool `json:"args_escaped"`
 	// Env populates the process environment for the process.
 	Env []string `json:"env,omitempty"`
 	// Cwd is the current working directory for the process and must be
 	// relative to the container's root.
 	Cwd string `json:"cwd"`
+}
+
+// User contains the user information for Windows
+// TODO Windows: Does this make sense?
+type User struct {
+	User string `json:"user,omitempty"`
 }
 
 // Root contains information about the container's root filesystem on the host.
@@ -84,6 +108,28 @@ type Mount struct {
 	Options []string `json:"options,omitempty"`
 }
 
+// HvRuntime contains settings specific to Hyper-V containers
+type HvRuntime struct {
+	// ImagePath is the path to the Utility VM image for this container
+	ImagePath string `json:"image_path,omitempty"`
+}
+
+// Networking contains the platform specific network settings for the container
+type Networking struct {
+	// These are here temporarily to maintain support for TP4
+	MacAddress string `json:"mac,omitempty"`
+	Bridge     string `json:"bridge,omitempty"`
+	IPAddress  string `json:"ip,omitempty"`
+	// PortBindings is the port mapping between the exposed port in the
+	// container and the port on the host.
+	PortBindings nat.PortMap `json:"port_bindings,omitempty"`
+
+	// Below this is what is needed for TP5 and going forward
+
+	// List of endpoints to be attached to the container
+	EndpointList []string `json:"endpoints,omitempty"`
+}
+
 // Hook specifies a command that is run at a particular event in the lifecycle of a container
 type Hook struct {
 	//	Path string   `json:"path"`
@@ -103,20 +149,52 @@ type Hook struct {
 //	Poststop []Hook `json:"poststop,omitempty"`
 //}
 
+// Storage contains storage resource management settings
+type Storage struct {
+	// Specifies maximum Iops for the system drive
+	Iops *uint16 `json:"iops,omitempty"`
+	// Specifies maximum bytes per second for the system drive
+	Bps *uint16 `json:"bps,omitempty"`
+}
+
+// Memory contains memory settings for the container
+type Memory struct {
+	// Memory limit (in bytes).
+	Limit *uint64 `json:"limit,omitempty"`
+	// Memory reservation (in bytes).
+	Reservation *uint64 `json:"reservation,omitempty"`
+}
+
+// CPU contains information for cpu resource management
+type CPU struct {
+	// Number of CPUs available to the container. This is an appoximation for Windows Server Containers.
+	Count *uint64 `json:"count,omitempty"`
+	// CPU shares (relative weight (ratio) vs. other containers with cpu shares). Range is from 1 to 10000.
+	Shares *uint64 `json:"shares,omitempty"`
+	// Percent of available CPUs usable by the container.
+	Percent *uint64 `json:"percent,omitempty"`
+}
+
+// Network network resource management information
+type Network struct {
+	// Bandwidth is the maximum egress bandwidth in bytes per second
+	Bandwidth *uint64 `json:"bandwidth,omitempty"`
+}
+
 // Resources has container runtime resource constraints
 // TODO Windows containerd. This structure needs ratifying with the old resources
 // structure used on Windows and the latest OCI spec.
 type Resources struct {
-	//	// Memory restriction configuration
-	//	Memory *Memory `json:"memory,omitempty"`
-	//	// CPU resource restriction configuration
-	//	CPU *CPU `json:"cpu,omitempty"`
-	//	// Task resource restriction configuration.
-	//	Pids *Pids `json:"pids,omitempty"`
-	//	// BlockIO restriction configuration
-	//	BlockIO *BlockIO `json:"blockIO,omitempty"`
-	//	// Network restriction configuration
-	//	Network *Network `json:"network,omitempty"`
+	// Memory restriction configuration
+	Memory *Memory `json:"memory,omitempty"`
+	// CPU resource restriction configuration
+	CPU *CPU `json:"cpu,omitempty"`
+	// Storage restriction configuration
+	Storage *Storage `json:"storage,omitempty"`
+	// Network restriction configuration
+	Network *Network `json:"network,omitempty"`
+	// Sandbox size indicates the size to expand the system drive to if it is currently smaller
+	SandboxSize *uint64 `json:"sandbox_size,omitempty"`
 }
 
 // HACK - below taken from execdriver\driver.go and driver_windows.go
