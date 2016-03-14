780383bccd37d3b61620ea8907c3d7ccfb965331
Tonis Tiigi <tonistiigi@gmail.com>
2016-03-14 15:02:48 -0700

    Replace execdrivers with containerd implementation
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>
    Signed-off-by: Kenfe-Mickael Laventure <mickael.laventure@gmail.com>
    Signed-off-by: Anusha Ragunathan <anusha@docker.com>

--- daemon/exec/exec.go
+++ daemon/exec/exec.go
@@ -1,11 +1,8 @@
 package exec
 
 import (
-	"fmt"
 	"sync"
-	"time"
 
-	"github.com/docker/docker/daemon/execdriver"
 	"github.com/docker/docker/pkg/stringid"
 	"github.com/docker/docker/runconfig"
 )
@@ -15,17 +12,22 @@ import (
 // examined both during and after completion.
 type Config struct {
 	sync.Mutex
+	sync.WaitGroup
 	*runconfig.StreamConfig
-	ID            string
-	Running       bool
-	ExitCode      *int
-	ProcessConfig *execdriver.ProcessConfig
-	OpenStdin     bool
-	OpenStderr    bool
-	OpenStdout    bool
-	CanRemove     bool
-	ContainerID   string
-	DetachKeys    []byte
+	ID          string
+	Running     bool
+	ExitCode    *int
+	OpenStdin   bool
+	OpenStderr  bool
+	OpenStdout  bool
+	CanRemove   bool
+	ContainerID string
+	DetachKeys  []byte
+	Entrypoint  string
+	Args        []string
+	Tty         bool
+	Privileged  bool
+	User        string
 
 	// waitStart will be closed immediately after the exec is really started.
 	waitStart chan struct{}
@@ -93,30 +95,4 @@ func (e *Store) List() []string {
 	}
 	e.RUnlock()
 	return IDs
-}
-
-// Wait waits until the exec process finishes or there is an error in the error channel.
-func (c *Config) Wait(cErr chan error) error {
-	// Exec should not return until the process is actually running
-	select {
-	case <-c.waitStart:
-	case err := <-cErr:
-		return err
-	}
-	return nil
-}
-
-// Close closes the wait channel for the progress.
-func (c *Config) Close() {
-	close(c.waitStart)
-}
-
-// Resize changes the size of the terminal for the exec process.
-func (c *Config) Resize(h, w int) error {
-	select {
-	case <-c.waitStart:
-	case <-time.After(time.Second):
-		return fmt.Errorf("Exec %s is not running, so it can not be resized.", c.ID)
-	}
-	return c.ProcessConfig.Terminal.Resize(h, w)
 }
